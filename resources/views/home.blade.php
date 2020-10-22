dabaccc12a42aaf8f04df61923d7fc62950c90cd
uusa35 <uusa35@gmail.com>
2020-10-22 13:32:09 +0300

    updates

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -50,15 +50,12 @@
                             {{ $elements->render() }}
                         </div>
                     @else
-                        <div class="jumbotron p-4 p-md-5 text-white rounded bg-danger">
-                            <div class="col-md-6 px-0">
-                                <h1 class="display-4 font-italic">{{ __('general.no_posts') }}</h1>
-                                <p class="lead my-3">Multiple lines of text that form the lede, informing new readers
-                                    quickly and efficiently about what’s most interesting in this post’s contents.</p>
-                                <p class="lead mb-0"><a href="#" class="text-white font-weight-bold">Continue
-                                        reading...</a></p>
+                            <div class="jumbotron p-4 p-md-5 text-white rounded bg-danger">
+                                <div class="col-md-12 px-0">
+                                    <h1 class="display-4 font-italic">{{ __('general.no_posts') }}</h1>
+                                    <p class="lead my-3">No Posts to preview.</p>
+                                </div>
                             </div>
-                        </div>
                     @endif
                 </div>
             </div>
